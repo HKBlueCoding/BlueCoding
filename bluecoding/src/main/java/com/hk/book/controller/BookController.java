f77a3cc77af0d985ba6c0254becaf4b7bba47577
bavsa <bavsa@192.168.219.100>
2021-12-09 10:46:34 +0900

    bookController

---
+++ bluecoding/src/main/java/com/hk/book/controller/BookController.java
@@ -0,0 +1,30 @@
+package com.hk.book.controller;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class BookController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(BookController.class);
+		
+	// 메인 페이지
+	@GetMapping("/")
+	public String home() {
+		
+		return "main";
+	}
+		
+}
