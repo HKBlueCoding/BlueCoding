d7887a2658a60b01cfa5b0239f284d79732d3407
jcaa0 <jcaa0@10870H5609>
2021-12-09 13:29:03 +0900

    notice,event

--- bluecoding/src/main/java/com/hk/news/controller/NewsController.java
+++ bluecoding/src/main/java/com/hk/news/controller/NewsController.java
@@ -2,6 +2,8 @@ package com.hk.news.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class NewsController {
@@ -10,6 +12,20 @@ public class NewsController {
 	public String newsAdd() {
 		// 11
 		return "newsAdd";
+	}
+	
+	@RequestMapping(value="/news/notice", method=RequestMethod.GET)
+	public String notice() {
+		
+		return "notice";
+	
+	}
+	
+	@RequestMapping(value="/news/event", method=RequestMethod.GET)
+	public String event() {
+		
+		return "event";
+	
 	}
 
 }
