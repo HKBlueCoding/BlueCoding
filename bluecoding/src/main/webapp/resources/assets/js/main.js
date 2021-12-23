8e4b6c2783c83f1edcdaa40ab5630a484b174711
jcaa0 <jcaa0@10870H5609>
2021-12-23 15:16:48 +0900

    src/main/webapp/resources/assets/js/main.js
    -> 각 게시판에서 스크롤 할 때마다 개발자모드 오류 카운트 올라가는 부분 주석처리

--- bluecoding/src/main/webapp/resources/assets/js/main.js
+++ bluecoding/src/main/webapp/resources/assets/js/main.js
@@ -9,15 +9,15 @@ Author: GrayGrids
 
 	//===== Prealoder
 
-	window.onload = function () {
-		window.setTimeout(fadeout, 200);
-	}
+//	window.onload = function () {
+//		window.setTimeout(fadeout, 200);
+//	}
 
 
-    function fadeout() {
-        document.querySelector('.preloader').style.opacity = '0';
-        document.querySelector('.preloader').style.display = 'none';
-    }
+//    function fadeout() {
+//        document.querySelector('.preloader').style.opacity = '0';
+//        document.querySelector('.preloader').style.display = 'none';
+//    }
 
 
     /*=====================================
@@ -34,12 +34,12 @@ Author: GrayGrids
         }
 
         // show or hide the back-top-top button
-        var backToTo = document.querySelector(".scroll-top");
-        if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-            backToTo.style.display = "flex";
-        } else {
-            backToTo.style.display = "none";
-        }
+//        var backToTo = document.querySelector(".scroll-top");
+//        if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+//            backToTo.style.display = "flex";
+//        } else {
+//            backToTo.style.display = "none";
+//        }
     };
 
      //===== mobile-menu-btn
